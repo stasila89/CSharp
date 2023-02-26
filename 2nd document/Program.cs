cab300efaa9c623c825900cf199ffb44a2248707
Stas Piskovski <stasila89@gmail.com>
2023-02-26 17:38:27 +0300

    added doc 2

---
+++ 2nd document/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите целое число: ");
+
+int userNumber = int.Parse(Console.ReadLine() ?? "");
+
+int result = userNumber * userNumber;
+
+Console.WriteLine($"{userNumber} -> {result}");
+
+
