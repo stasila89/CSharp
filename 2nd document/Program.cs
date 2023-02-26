9cc75ab232cb81fa513b1b8a8f47fb372610facb
Stas Piskovski <stasila89@gmail.com>
2023-02-26 17:43:04 +0300

    added 3rd Document

--- 2nd document/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.Write("Введите целое число: ");
-
-int userNumber = int.Parse(Console.ReadLine() ?? "");
-
-int result = userNumber * userNumber;
-
-Console.WriteLine($"{userNumber} -> {result}");
-
-
---
+++ 2ndDocument/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+Console.Write("Введите целое число: ");
+
+int userNumber = int.Parse(Console.ReadLine() ?? "");
+
+int result = (int)Math.Pow(userNumber, 2);
+
+Console.WriteLine($"{userNumber} -> {result}");
+
+
