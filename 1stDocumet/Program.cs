d197d2fca19b77bebf930f01cdc63be8953b2bac
Stas Piskovski <stasila89@gmail.com>
2023-02-26 16:05:15 +0300

    Initional commit

---
+++ 1stDocumet/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.SqlTypes;
+Console.Write("Введите целое число: ");
+
+string userInputStr = Console.ReadLine() ?? "";
+
+int userNumber = int.Parse(userInputStr);
+
+Console.WriteLine(userInputStr);
+
+
