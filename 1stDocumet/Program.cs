05559e8665eadc3176138c6ca2b663dd135a0ff3
Stas Piskovski <stasila89@gmail.com>
2023-02-26 17:16:13 +0300

    1st document

--- 1stDocumet/Program.cs
+++ 1stDocumet/Program.cs
@@ -1,10 +1,10 @@
-﻿using System.Data.SqlTypes;
-Console.Write("Введите целое число: ");
+﻿Console.Write("Введите целое число: ");
 
 string userInputStr = Console.ReadLine() ?? "";
-
 int userNumber = int.Parse(userInputStr);
 
-Console.WriteLine(userInputStr);
+int result = userNumber * userNumber;
+
+Console.WriteLine($"{userNumber} -> {result}");
 
 
