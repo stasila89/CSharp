32d9dffb8a9076150438ce43d0716c98ede46642
Stas Piskovski <stasila89@gmail.com>
2023-02-27 15:09:20 +0300

    added Practice2

---
+++ Practice2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите любое целое число от 1 до 7:");
+int Day = int.Parse(Console.ReadLine() ?? "");
+
+if ((Day<1) | (Day>7))
+{
+    Console.Write("Ошибка");
+}
+else
+{
+    if (Day==1) { Console.Write("Понедельник"); }
+    if (Day==2) { Console.Write("Вторник"); }
+    if (Day==3) { Console.Write("Среда"); }
+    if (Day==4) { Console.Write("Четверг"); }
+    if (Day==5) { Console.Write("Пятница"); }
+    if (Day==6) { Console.Write("Суббота"); }
+    if (Day==7) { Console.Write("Воскресенье"); }
+}
