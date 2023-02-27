8f47df33c7d2a1c6e64925a90648e3ece49e295a
Stas Piskovski <stasila89@gmail.com>
2023-02-27 14:48:48 +0300

    added practice 1

--- 3rdDocument/Program.cs
+++ 3rdDocument/Program.cs
@@ -3,7 +3,7 @@ Console.Write("Введите целое число: ");
 
 int userNumber = int.Parse(Console.ReadLine() ?? "");
 
-int result = (int)Math.Pow(userNumber, 2);
+double result = (int)Math.Pow(userNumber, 2);
 
 Console.WriteLine($"{userNumber} -> {result}");
 
---
+++ Practice1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+int userNumber1 = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine("Введите второе число: ");
+int userNumber2 = int.Parse(Console.ReadLine() ?? "");
+if (userNumber1 == userNumber2 * userNumber2)
+{
+    Console.WriteLine("Да");
+}
+    else
+{
+    Console.WriteLine("Нет");
+}
+//Console.WriteLine($"{userNumber1} , {userNumber2}");
+
+
